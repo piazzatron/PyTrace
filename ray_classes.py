f72a931e9d66bfb5c38ca5c51d4cf7f04c8bc960
Michael Piazza <michael.piazza.mp@gmail.com>
2014-08-21 11:41:51 -0700

    Full PyCharm Project

--- ray_classes.py
+++ ray_classes.py
@@ -2,27 +2,42 @@
 from PIL import Image, ImageDraw
 import math
 
-sceneObjList=[]
+
+
 class SceneObj(object):
     ##this is just a superclass of Sphere currently, but will be used to eventually house all renderable objects
-    def __init__(self): 
+    def __init__(self):
         sceneObjList.append(self)
 
+class Light(object):
+    def __init__(self, position):
+        self.position=position
+
+class Pixel(object):
+    def __init__(self, x, y):
+        self.x = x
+        self.y = y
+
+    def __str__(self):
+        return "("+str(self.x)+","+str(self.y)+")"
 class Sphere(SceneObj):
-    def __init__(self,radius,center):
-        self.radius=radius
-        self.center=center
+    def __init__(self,radius, center, color, ambColor = (0, 0, 0)):
+        self.radius = radius
+        self.center = center
+        self.color = color
+        self.ambient = ambColor
     def getRadius(self):
         return self.radius
     def getCenter(self):
         return self.center
-    
+
+
 class Canvas(object):
-    def __init__(self,height,width):
-        self.height=height
-        self.width=width
-        self.img=Image.new("RGB",(width,height),"white")
-        self.draw=ImageDraw.Draw(self.img)
+    def __init__(self, width, height):
+        self.height = height
+        self.width = width
+        self.img = Image.new("RGB",(width,height),"white")
+        self.draw = ImageDraw.Draw(self.img)
     def getHeight(self):
         return self.height
     def getWidth(self):
@@ -31,52 +46,74 @@ class Canvas(object):
         return self.img
     def getDraw(self):
         return self.draw
-    
+
+class ImagePlane(object):
+    def __init__(self,position,height,width):
+        self.position=position
+        self.height=height
+        self.width=width
+
+class Camera(object):
+    def __init__(self, position, direction):
+        self.direction = direction
+        self.position = position
+
+    def setPosition(self,otherVec3):
+        self.position = otherVec3
+
+    def setDirection(self,otherVec3):
+        self.direction=otherVec3
+
+
 class Ray(object):
     def __init__(self,Po,rDir):
-        self.Po=Po
-        self.rDir=rDir
+        self.Po = Po
+        self.rDir = rDir
     def getOrigin(self):
-        print self.Po
         return self.Po
     def getDirection(self):
-        print self.rDir
         return self.rDir
     def getPoint(self,t):
         return (self.Po + self.rDir*t)
-    
+
+
 class Vec3(object):
     def __init__(self,x,y,z):
-        self.x=x
-        self.y=y
-        self.z=z
+        self.x = x
+        self.y = y
+        self.z = z
     def __str__(self):
+
         return str(self.x)+","+str(self.y)+","+str(self.z)
     def __add__(self,other):
+
         return Vec3(self.x+other.x,self.y+other.y,self.z+other.z)
     def __sub__(self,other):
+
         if type(other)==Vec3:
             return Vec3(self.x-other.x,self.y-other.y,self.z-other.z)
     def __mul__(self,other):
+
         if type(other)==Vec3:
             return Vec3(self.x*other.x, self.y*other.y,self.y*other.z)
         else:
             return Vec3(self.x*other, self.y*other, self.z*other)
     def __div__(self,other):
-        if(type(other)==Vec3):
+
+        if type(other)==Vec3:
             return Vec3(self.x/other.x, self.y/other.y, self.z/other.z)
         else:
-            inv=(1.0/other)
+            inv = (1.0/other)
             return Vec3(self.x*inv, self.y*inv, self.z*inv)
     def getMagnitude(self):
         return math.sqrt(self.x**2+self.y**2+self.z**2)
     def normalize(self):
-        mag=self.getMagnitude()
+        mag = self.getMagnitude()
         if mag==0:
-            return 
+            return
         else:
-            inverseMag=(1/mag) ##a little optimization, so we're multiplying by inverse instead of dividing each time
-            normalVec=Vec3(self.x*inverseMag,self.y*inverseMag,self.z*inverseMag)
+            inverseMag = (1/mag) ##a little optimization, so we're multiplying by inverse instead of dividing each time
+            normalVec = Vec3(self.x*inverseMag,self.y*inverseMag,self.z*inverseMag)
             return normalVec
     def dot(self,other):
         return self.x*other.x + self.y*other.y + self.z*other.z
@@ -87,24 +124,26 @@ class Vec3(object):
             self.x*other.y - self.y * other.x
             )
     def getAngle(self,other):
-        selfNorm=self.normalize()
-        otherNorm=other.normalize()
+        selfNorm = self.normalize()
+        otherNorm = other.normalize()
         return math.degrees(math.acos(selfNorm.dot(otherNorm)))
-    
+
 class Matrix44(object):
+
+
     def __init__(self, r0,r1,r2,r3):
-        self.r0=r0
-        self.r1=r1
-        self.r2=r2
-        self.r3=r3
-        self.m=[self.r0, self.r1, self.r2, self.r3]
+        self.r0 = r0
+        self.r1 = r1
+        self.r2 = r2
+        self.r3 = r3
+        self.m = [self.r0, self.r1, self.r2, self.r3]
     def __mul__(self,other):
         if type(other)==Matrix44:
             print ("ees two matricies")
-            prodM=Matrix44([0]*4,[0]*4,[0]*4,[0]*4)
+            prodM = Matrix44([0]*4,[0]*4,[0]*4,[0]*4)
             for row in range(4):
                 for column in range(4):
-                    prodM.m[row][column]=(
+                    prodM.m[row][column] = (
                         self.m[row][0]*other.m[0][column]+
                         self.m[row][1]*other.m[1][column]+
                         self.m[row][2]*other.m[2][column]+
--- ray_tracer.py
+++ ray_tracer.py
@@ -1,108 +1,244 @@
 #!/Users/michaelpiazza/Documents/RayTracer/
 from ray_classes import *
-
+import random
+import sys
+import time
+##vars
+objectList = []
+lightList = []
 ##functions
-    
+
+def addLight (position):
+    light = Light
+    light.position = position
+    lightList.append(light)
+
+def addSphere(center,radius,color, ambientColor = (0, 0, 0)):
+    """
+
+    :rtype : object
+    """
+    sphere = Sphere(radius,center,color, ambientColor)
+    objectList.append(sphere)
+
 def solveQuadratic(pOrig,rDir,center,radius): ##I'm not implementing this with the fancy optimized function, if shit's broken look to this
-    a=rDir.dot(rDir) ##this is the direction of the ray, squared
-    b=rDir.dot((pOrig-center))*2.0
-    c=(((pOrig-center).getMagnitude()**2.0)-radius**2.0)
-    disc=(b**2)-(a*c*4.0) ##evaluate discriminant
+    a = rDir.dot(rDir) ##this is the direction of the ray, squared
+    b = rDir.dot((pOrig-center))*2.0
+    c = (((pOrig-center).getMagnitude()**2.0)-radius**2.0)
+    disc = (b**2)-(a*c*4.0) ##evaluate discriminant
     if disc<0:
-        print("no hit")
-        print disc
         return False
         
     elif disc==0:
-        print("one hit")
         return #(b/(2.0*a))*-1.0
     else:
-        print("two hits!")
-        print ("discriminant is "+str(disc))
-        solutions=((((b*-1.0)+math.sqrt(disc))/(a*2.0)),(((b*-1.0)-math.sqrt(disc))/(a*2.0)))
-        print "solutions for t val of ray are: " + str(solutions)
+        solutions = ((((b*-1.0)+math.sqrt(disc))/(a*2.0)),(((b*-1.0)-math.sqrt(disc))/(a*2.0)))
         return solutions
     
 def sphereRayIntersect(sphere,ray):
-    intersections=[]
-    tVal=solveQuadratic(ray.getOrigin(),ray.getDirection(),sphere.getCenter(),sphere.getRadius())
-    for each in tVal:
-        intersections.append(ray.getPoint(each))
-        print "intersection point at " + str(ray.getPoint(each))
-    return intersections
-#draws noise of alterating black and white across the image. for testing drawing
+
+    # intersections = []
+    # tVal = solveQuadratic(ray.getOrigin(),ray.getDirection(),sphere.getCenter(),sphere.getRadius())
+    # for each in tVal:
+    #     intersections.append(ray.getPoint(each))
+    solution = solveQuadratic(ray.getOrigin(),ray.getDirection(),sphere.getCenter(),sphere.getRadius())
+    if solution == False:
+        return False
+    parametricValue = min(solution)
+    #because solveQuadratic will often return two values, we want to take the minimum (closest) of these two values
+    return parametricValue
+
+
+
+#draws noise of alternating black and white across the image. for testing drawing
 #functions.
 def drawNoise(canvas):
-    color=0
-    height=canvas.getHeight()
-    width=canvas.getWidth()
+    color = 0
+    height = canvas.getHeight()
+    width = canvas.getWidth()
     for x in range(height):
-        if color==0: #this makes it so that each line starts as a new color
-            color=1
+        if color == 0: #this makes it so that each line starts as a new color
+            color = 1
         else:
-            color=0
+            color = 0
         for y in range(width):
-            if color==0:
+            if color == 0:
                 canvas.getDraw().point((x,y), (0,0,0))
-                color=1
+                color = 1
             else:
                 canvas.getDraw().point((x,y), (255,255,255))
-                color=0
+                color = 0
     canvas.getImg().save("noiseIMG.png","PNG")
     canvas.getImg().show()
     print("I finished drawing your noise image, Michael!")
     return
-def render(height,width):
-    canvas = Canvas(height,width)
-    for x in width:
-        for y in height:
-            #make ray
-            canvas.getDraw.point(x,y,trace)
+
+def rasterToCamera(pixel, width, height,fov):
+    # Takes a pixel from the canvas in canvas dimensions, and returns that point in worldSpace
+    assert(isinstance(pixel,Pixel))
+    aspectRatio = float(width) / float(height)
+    fovRadian = math.radians(fov)
+
+    def rasterToNormalized(point):
+        #Normalizes the coord of the canvas pixel in a plane going from (0,1)
+        pixelX = (point.x + 0.5) / width
+        pixelY = (point.y + 0.5) / height
+        #we add 0.5 to shoot it thru the middle of the pixel
+        return Pixel(pixelX,pixelY)
+
+    def normalizedToScreen(point):
+        #remaps the normalized into a range of (-1,1) multiplied by the FOV
+        remappedX = (2.0 * point.x - 1.0) * math.tan(fovRadian/2.0)
+        remappedY = (1.0 - (2.0 * point.y)) * math.tan(fovRadian/2.0)
+        if aspectRatio != 1:
+            remappedX *= aspectRatio
+        return Pixel(remappedX,remappedY)
+
+    transformedPixel = normalizedToScreen(rasterToNormalized(pixel))
+    return Vec3(transformedPixel.x, transformedPixel.y, -1.0)
+
+def render(width, height, camera, objList):
+    startTime = time.clock()
+    canvas = Canvas(width,height)
+    #loops through all pixels in the canvas, column by column
+    for x in range(width):
+        print (float(x)*100.0)/float(width)
+        for y in range(height):
+            computedPoint = rasterToCamera(Pixel(x,y), width, height,60.0)
+            #takes the point in
+            canvas.getDraw().point((x,y), trace(camera.position, computedPoint, objList))
+    endTime = time.clock()
+    print "Total time was  " + str(endTime - startTime)
+    canvas.getImg().save("tracedImg.png","PNG")
+    canvas.getImg().show()
+
 def makeScene():
     return
-def trace():
-    return #color
-def shade():
-    return color
-def run():
+
+def trace(cameraOrigin, imagePlanePoint,objList):
+
+    collisionList=[]
+    #We get the directional vector from the camera to the img plane point, and normalize
+    directionVec = (imagePlanePoint-cameraOrigin).normalize()
+    #We make the ray we're gonna trace
+    traceRay = Ray(cameraOrigin,directionVec)
+    #We test each object to see if it hits the traceray, and store only the closest value
+    for sceneObj in objList:
+        intersectionPoint = sphereRayIntersect(sceneObj,traceRay)
+        if intersectionPoint != False:
+            if len(collisionList)==0:
+                collisionList.append(sceneObj)
+                collisionList.append(intersectionPoint)
+            elif intersectionPoint < collisionList[1] and intersectionPoint > 0:
+                # print("old val was" + str(collisionList[1]) + "new val is " + str(intersectionPoint))
+                collisionList[0], collisionList[1] = sceneObj, intersectionPoint
+    if len(collisionList)>0:
+        return shade(collisionList[0], traceRay.getPoint(collisionList[1]))
+        #call the shade function with the neareest object, and corresponding intersection point
+    return "#FFFFFF"
+#take cameraOrigin, imagePlanePoint
+#make a normalized direction vector from the cameraOrigin to the imagePlanePoint
+#make a ray, with startpoint as origin and normalized direction vector
+#for every object in the scene, loop thru the sphereIntersect function w/it and the ray
+#if it hits, return the color of that sphere
+#if not, return black
+def shadowTest(ray):
+    for object in objectList:
+        #if the ray hits any object, return true: the object is in shadow. So if intersect != False, because intersect never returns true, only false, or a paremets
+        intersection = sphereRayIntersect(object,ray)
+        if intersection > 0:
+            return True
+    else:
+        # print"didn't hit"
+        return False
+
+
+def shade(object, point):
+
+
+    def tupToList(myTuple):
+        return [x for x in myTuple]
+
+    def intTuple(myTuple):
+        tupList = [int(x) for x in myTuple]
+        return (tupList[0], tupList[1], tupList[2])
+
+    def lambertShade(color, ambient, angle):
+        colorList = [x for x in color]
+        colorList = [channel * angle for channel in colorList]
+        for x in colorList:
+            if x<0:
+                sys.exit()
+        return colorList
+
+    def ambientAdjustment(color, ambient):
+        #incorporate some scale factors 'n shit
+        for i in range(len(color)):
+            #first, we avoid a divide by zero error
+            if color[i] == 0:
+                color[i]=ambient[i]
+            else:
+                #if it isn't black, we find out the scale of the thing  away from the ambient color
+                scaleFactor = (255.0-float(ambient[i]))/255.0
+                color[i] = (color[i] * scaleFactor) + ambient[i]
+        return color
+
+
+
+    ambient = tupToList(object.ambient)
+
+    for light in lightList:
+        #this is the direction from the hit point of the ray, and the light
+        directionVector = light.position - point
+        #the normal off of this point in the sphere
+        normalVector = point - object.getCenter()
+        directionVector = directionVector.normalize()
+        normalVector = normalVector.normalize()
+        shadowRay = Ray(point, directionVector)
+        if shadowTest(shadowRay):
+            return object.ambient
+        #determine the project of one normalized vector onto the other, from 0-1
+        angle = normalVector.dot(directionVector)
+        if angle < 0:
+            angle = 0
+        #convert some RGB values, scaled from 0-255
+        newColor = lambertShade(object.color, object.ambient, angle)
+        newColor = ambientAdjustment(newColor,ambient)
+        return intTuple(newColor)
+    # should return a color
+
+def run(height,width):
+
     makeScene()
-    
+    camera = Camera(Vec3(0, 0, 0),Vec3(0, 0, -1.0))
+    imagePlane = ImagePlane(Vec3(0, 0, -1.0), 1.0, 1.0)
+    render(height,width)
+
+def populateScene(numSpheres):
+    colorList = [[(255,107,84),(90,50,50)], [(66,90,245), (45, 45, 80)], [(45,250,85), (40,70,70)],[(255, 156, 226),(128, 94, 118)],[(213, 255, 87),(122, 130, 98)]]
+    for x in range(numSpheres):
+        xCoor = random.randrange(-160, 160)
+        yCoor = random.randrange(-100, 100)
+        zCoor = random.randrange(-400, -100)
+        radius = int(zCoor * -0.1 * random.uniform(1,3))
+        color = random.choice(colorList)
+        addSphere(Vec3(xCoor,yCoor,zCoor),radius, color[0], color[1])
+addLight (Vec3(-75, 200, -100))
+
+# addSphere(Vec3(-40, 0, -150), 75, (255,107,84), (90,50,50)) #red sphere
+# addSphere(Vec3(0, 200, -400), 100, (66,90,245), (45, 45, 80)) #blue sphere
+# addSphere(Vec3(-40,75,-70), 25, (45,250,85), (40,70,70)) #green sphere
+
+populateScene(10)
+def micky():
+    addSphere(Vec3(0,-75.0,-600.0), 150.0, (200, 200, 200))#head
+    addSphere(Vec3(-55,-60.0,-450.0), 80.0, (200, 200, 200))#muzzle
+    addSphere(Vec3(-100,-25.0,-370.0), 30.0, (200, 200, 200))#nose
+    addSphere(Vec3(30.0,145.0,-700.0), 140.0, (200, 200, 200)) #left ear
+    addSphere(Vec3(260.0,-25,-700.0), 140.0, (200, 200, 200)) #right ear
 
 
-#operations
-##vec1=Vec3(2.0,4.0,6.0)
-##vec2=Vec3(1.0,2.0,3.0)
-##print vec1+vec2
-##print vec1-vec2
-##print vec1*vec2
-##print vec1*5
-##print vec1/4
-##print vec1/vec2
-##M1=Matrix44([1,2,3,4],[1,2,3,4],[1,2,3,4],[1,2,3,4])
-##M2=Matrix44([1,2,3,4],[1,2,3,4],[1,2,3,4],[1,2,3,4])
-##M3=M1*M2
-##print M3
-ray=Ray(Vec3(-10.0,0,0),Vec3(1.0,0.0,0.0))
-sphere=Sphere(3.0,Vec3(0,0,0))
-print sphereRayIntersect(sphere,ray)
-ray2=Ray(Vec3(0,0,0),Vec3(0,1.0,0))
-sphere2=Sphere(4.0, Vec3(0,50,0))
-print sphereRayIntersect(sphere2,ray2)
-##print(math.sqrt(9))
-##myVec=Vec3(1,2,3)
-##myVec.getMagnitude()
-##
-##drawNoise(Canvas(300,300))
-##sphere1=Sphere(5,(0,0))
-##print sceneObjList
-##print sceneObjList[0].getPosition()
-##vec1=Vec3(5,0,0)
-##vec2=Vec3(0,5,0)
-##vec3=vec1.cross(vec2)
-##print vec3
-
-
-
-##Raytracer
+# micky()
+render(500,500,Camera(Vec3(0, 0, 0), Vec3(0, 0, -1)),objectList)
 
 
